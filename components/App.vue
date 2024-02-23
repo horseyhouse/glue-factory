9d430ef77bc62bef1df3ed79d649b72ca3b21db5
theia <theia@mit.edu>
2024-02-23 15:06:00 -0500

    added flyers

--- components/App.vue
+++ components/App.vue
@@ -1,10 +1,14 @@
-<script lang="ts">
+<script setup lang="ts">
+import Flyers from './Flyers.vue'
 </script>
 
 <template>
     <h1>
         The Glue Factory
     </h1>
-    <h2>Somerville, MA</h2>
+    <p>Somerville, MA</p>
     <p>ask a horse</p>
+
+    <h2>Past Shows</h2>
+    <Flyers />
 </template>
---
+++ components/Flyers.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+const paths = Object.keys(import.meta.glob("/media/flyers/*"));
+
+function pathToDate(path: string): Date {
+    // Regex that extracts the filename from the path, without extension
+    const filename = path.replace(/^.*[\\\/]/, '').split('.').slice(0, -1).join('.')
+
+    return new Date(filename)
+}
+
+const pathsAndDates = paths
+    // Compute a date for each path
+    .map((path: string) => ({
+        path,
+        date: pathToDate(path)
+    }))
+    // Sort by the date
+    .sort((a, b) => b.date.getTime() - a.date.getTime())
+</script>
+
+<template>
+    <ul>
+        <li v-for="{ path, date } of pathsAndDates">
+            <figure>
+                <caption>
+                    {{ date.toLocaleDateString(undefined, {
+                        weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'
+                    }) }}
+                </caption>
+                <img :src="path">
+            </figure>
+        </li>
+    </ul>
+</template>
+
+<style>
+ul {
+    list-style: none;
+    display: flex;
+    gap: 1rem;
+    flex-wrap: wrap;
+}
+
+li {
+    flex: 1;
+    padding: 1rem;
+    border: 1px solid #ccc;
+}
+
+figure {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    min-width: 11rem;
+}
+
+img {
+    width: 100%;
+    height: auto;
+}
+
+caption {
+    text-align: center;
+    width: 100%;
+}
+</style>
