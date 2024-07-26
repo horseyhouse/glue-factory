ece42d61289207357ee8ec9af947a99170526486
theia <theia@mit.edu>
2024-07-26 14:42:22 -0400

    added flyer

--- components/App.vue
+++ components/App.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
-import Flyers from './Flyers.vue'
+import Flyers from "./Flyers.vue";
 </script>
 
 <template>
-    <h1>
-        The Glue Factory
-    </h1>
+    <h1>The Glue Factory</h1>
     <p>Somerville, MA</p>
     <p>ask a horse</p>
 
-    <h2>Past Shows</h2>
+    <h2>Shows</h2>
     <Flyers />
 </template>
